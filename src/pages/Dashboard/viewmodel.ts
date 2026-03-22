8c0c6b981147a9097fb2ed27a8d46b6ea8581c54
Babatunde Ojo <babatundeo@theluxev.com>
2026-03-22 15:12:40 +0100

    simulate loading

--- src/pages/Dashboard/viewmodel.ts
+++ src/pages/Dashboard/viewmodel.ts
@@ -1,11 +1,22 @@
+import { useState, useEffect } from "react";
 import { MockDashboardData } from "@/fixtures";
 
 function useDashboardViewModel() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const { leads, revenue, retentionRate, customers, tasks, insight } =
     MockDashboardData;
 
   return {
-    isLoading: true,
+    isLoading,
     leads,
     revenue,
     retentionRate,
